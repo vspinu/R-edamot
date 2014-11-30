93f3b3f37d643c2339577769a9dd17fb0fba8e3d
Vitalie Spinu <spinuvit@gmail.com>
2014-11-30 03:31:08 -0800

    Add "fetch" and "ns" middlewares

--- R/middlewares.R
+++ R/middlewares.R
@@ -6,17 +6,17 @@ source("~/VC/R-edamot/R/utils.R")
 ## INFO
 mw_info <- middleware("info", handles = c("info"),
                       fun = function(h){
-                          function(op, transport, symbol = NULL, ns = NULL, ...){
+                          function(op, tr, symbol = NULL, ns = NULL, ...){
                               msg <- list(...)
                               if (op == "info"){
-                                  obj <- get_in_ns(symbol, ns, transport)
+                                  obj <- get_in_ns(symbol, ns)
                                   info <- object_info(obj)
-                                  transport$write(respfor(msg, ns = ns, symbol = symbol,
-                                                          status = list("done"),
-                                                          lst = info))
+                                  tr$write(respfor(msg, ns = ns, symbol = symbol,
+                                                   status = list("done"),
+                                                   lst = info))
                               } else {
                                   msg <- nREPL:::assoc(
-                                      msg, op = op, transport = transport,
+                                      msg, op = op, tr = tr,
                                       symbol = symbol, ns = ns,
                                       non_null = TRUE)
                                   do.call(h, msg)
@@ -38,18 +38,18 @@ assign("info", mw_info, envir = nREPL::middlewares)
 ### FETCH
 mw_fetch <- middleware("fetch", handles = c("fetch"),
                        fun = function(h){
-                           function(op, transport, symbol = NULL, ns = NULL, recursive = NULL, ...){
+                           function(op, tr, symbol = NULL, ns = NULL, recursive = NULL, ...){
                                msg <- list(...)
                                if (op == "fetch"){
                                    if (is.null(recursive)) recursive <- TRUE
-                                   obj <- get_in_ns(symbol, ns, transport)
+                                   obj <- get_in_ns(symbol, ns)
                                    info <- struct_encode(obj, recursive = recursive)
-                                   transport$write(respfor(msg, ns = ns, symbol = symbol,
-                                                           status = list("done"),
-                                                           lst = info))
+                                   tr$write(respfor(msg, ns = ns, symbol = symbol,
+                                                    status = list("done"),
+                                                    lst = info))
                                } else {
                                    msg <- nREPL:::assoc(
-                                       msg, op = op, transport = transport,
+                                       msg, op = op, tr = tr,
                                        symbol = symbol, ns = ns, recursive = recursive, 
                                        non_null = TRUE)
                                    do.call(h, msg)
@@ -66,3 +66,35 @@ assign("fetch", mw_fetch, envir = nREPL::middlewares)
 ## test_middleware(mw_info, op = "info", symbol = "plot", ns = "iris")
 ## test_middleware(mw_info, op = "info", symbol = "Sepal.Length", ns = "iris")
 ## test_middleware(mw_info, op = "info", symbol = ".GlobalEnv", ns = "namespace:base")
+
+
+### NS
+mw_ns <- middleware("ns", handles = c("ns-list", "ns-vars"),
+                    fun = function(h){
+                        function(op, tr, ns = NULL, pattern = NULL, type = NULL, ...){
+                            msg <- list(...)
+                            if( op %in% c("ns-list", "ns-vars")){
+                                value <- switch(op,
+                                                'ns-vars' = ns_vars(pattern, ns, type),
+                                                'ns-list' = ns_list(pattern, type))
+                                tr$write(respfor(msg, ns = ns, type = type,
+                                                 status = list("done"),
+                                                 value = as.list(value)))
+                            } else {
+                                msg <- nREPL:::assoc(
+                                    msg, op = op, tr = tr,
+                                    ns = ns, pattern = pattern, type = type, 
+                                    non_null = TRUE)
+                                do.call(h, msg)
+                            }
+                        }
+                    })
+assign("ns", mw_ns, envir = nREPL::middlewares)
+
+## test_middleware(mw_ns, op = "ns-list")
+## test_middleware(mw_ns, op = "ns-list", type = "package")
+## test_middleware(mw_ns, op = "ns-list", pattern = "sta", type = "package")
+## test_middleware(mw_ns, op = "ns-list", pattern = "middddd", type = "environment")
+## test_middleware(mw_ns, op = "ns-list", pattern = "mid", type = "environment")
+## test_middleware(mw_ns, op = "ns-list", pattern = "ir", type = "list")
+## test_middleware(mw_ns, op = "ns-vars", ns = "iris")
--- R/utils.R
+++ R/utils.R
@@ -8,20 +8,26 @@ get_ns <- function(ns){
     else {
         if (exists(ns, envir = globalenv()))
             get(ns, envir = globalenv())
-        else
+        else {
             backToTopError("No such ns: %s", ns)
+            NULL
+        }
     }
 }
 
-get_in_ns <- function(symbol, ns, transport){
+get_in_ns <- function(symbol, ns){
+    if(is.null(ns)){
+        ns <- ".GlobalEnv"
+        inher <- TRUE
+    } else
+        inher <- FALSE
     symbol <- as.character(symbol)
     ns_obj <- get_ns(ns)
     if(is.environment(ns_obj)){
-        if (exists(symbol, ns_obj, inherits = FALSE)){
+        if (exists(symbol, ns_obj, inherits = inher))
             get(symbol, envir = ns_obj)
-        } else {
+        else
             backToTopError("Object '%s' does not exist in '%s'", symbol, ns)
-        }
     } else ns_obj[[symbol]]
 }
 
@@ -42,8 +48,45 @@ struct_encode <- function(obj, recursive = TRUE){
     info <- object_info(obj)
     if (recursive && is.recursive(obj))
         ## do not convert to bendict here. If no names, leave 
-        obj <- lapply(obj, struc_encode, recursive = recursive)
+        obj <- lapply(obj, struct_encode, recursive = recursive)
     bendict(info = info, data = obj)
 }
+
+ns_vars <- function (pattern = NULL, ns = NULL, mode = NULL) {
+    if (is.null(pattern)) pattern <- ".*"
+    if (is.null(mode)) mode <- "any"
+    accum <- character(0L)
+    .ls1 <- function(ns_obj){
+        li <- grep(pattern, ls(pos = ns_obj, all.names = TRUE), value = TRUE)
+        if (length(li) > 0 && mode != "any")
+            li <- li[sapply(li, exists, where = ns_obj, inherits = FALSE, mode = mode)]
+        li
+    }
+    out <-
+        if (!is.null(ns))
+            .ls1(get_ns(ns))
+        else
+            unlist(lapply(search(), .ls1), use.names=F)
+    sort(out)
+}
+
+
+ns_list <- function (pattern = NULL, type = NULL){
+    if(is.null(pattern)) pattern <- ".*"
+    if(is.null(type)) type <- "search"
+    switch(type,
+           search = grep(pattern, search(), value = T),
+           package = grep(pattern, grep("^package:", search(), value = T), value = T), 
+           namespace = paste0("namespace:", grep(pattern, loadedNamespaces(), value = T)), 
+           environment = ns_vars(pattern = pattern, mode = "environment"),
+           list = ns_vars(pattern = pattern, mode = "list"),
+           backToTopError("Invalid `type` argument (%s).\n Accepted types are 'search', 'package', 'namespace', 'environment' and 'list'.",
+                          type))
+}
+
+## ns_list(typ = "namespace");
+## ns_list(typ = "package")
+## ns_list("[z].*", type = "environment")
+## ns_list("^[a-z].*", type = "environment")
 
 
